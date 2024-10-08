57bab3e43874a1f010882a2a866b3b2dc23a5b00
RG <itsrohitgiri@gmail.com>
2024-10-08 15:33:50 +0530

    js

---
+++ commonmain.js
@@ -0,0 +1,5 @@
+const check = require("./common.js");
+console.log("Add: ", check.add(5,3));
+console.log("Subtract: ", check.subtract(5,3));
+console.log("PI: ", check.PI);
+console.log("Multiply: ", check.multiply(5,3));
